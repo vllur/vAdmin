5c2cc53465085e50a068677d14903f7ff3aa3aad
vellur <veller950@gmail.com>
2018-07-06 21:28:42 +0200

    Added memberlist

---
+++ src/memberlist.lua
@@ -0,0 +1,12 @@
+-- MEMBERS LISTS
+-- If you want to give rank to user, add his/her USGN ID to the list
+-- Example:
+-- ownerlist = {132465}
+-- smod list = {123456, 654321}
+-- and so on...
+
+ownerlist = {}					--Owner
+adminlist = {}					--Admin
+smodlist = {}					--S-mod
+modlist = {}					--Mod
+viplist = {}					--VIP
